287285366f46a65cec3d5e1124b723911112c67f
nikolasmdr08 <69701191+nikolasmdr08@users.noreply.github.com>
2022-07-13 21:29:48 -0300

    correccion modal

--- JS/script.js
+++ JS/script.js
@@ -20,10 +20,6 @@ class ItemCarrito{
         this.cantidad = cantidad;
         this.precioUnitario = precioUnitario;
     }
-
-    precioConIVA(){
-        return this.precioUnitario * 1.21
-    }
 }
 
 window.onload = () => {
@@ -51,6 +47,16 @@ window.onload = () => {
                 agregarItemAModal(elemnto.id, elemnto.descripcion, elemnto.cantidad, elemnto.precioUnitario)  
             }
         )
+        let total = 
+        `
+        <li class="listItems">
+            <div colspan="2"></div>
+            <span class="restricion"> TOTAL </span>
+            <span class="restricion">$${calcularTotal()} </span>
+        </li>
+        `
+        let modalCarrito = document.getElementById("list");
+        modalCarrito.innerHTML += total;
     }
     else{
         document.getElementById("list").innerHTML = "No se ha seleccionado ningun producto. 😢"
@@ -149,6 +155,16 @@ function agregarItem(idItem, descripcion, cantidadItem, precioItem){
             agregarItemAModal(elemnto.id, elemnto.descripcion, elemnto.cantidad, elemnto.precioUnitario)  
         }
     )
+    let total = 
+    `
+    <li class="listItems">
+        <div colspan="2"></div>
+        <span class="restricion"> TOTAL </span>
+        <span class="restricion">$${calcularTotal()} </span>
+    </li>
+    `
+    let modalCarrito = document.getElementById("list");
+    modalCarrito.innerHTML += total;
     // alerta ok
     Swal.fire({
         icon: 'success',
@@ -162,18 +178,57 @@ function agregarItemAModal(idItem, descripcion, cantidadItem, precioItem){
     let item = 
         `<li class="listItems">
             <span class="restricion" id="carList_nombre">${descripcion}</span>
-            <input class="restricion" type="number" name="cant" id="carList_cant" value="${cantidadItem}">
+            <input class="restricion" type="number" name="cant" id="cant_${idItem}" onchange="editarCantidad('cant_${idItem}','${idItem}')" value="${cantidadItem}">
             <span class="restricion" id="carList_precio">$${(precioItem*monedaActual).toFixed(2)}</span>
-            <a onclick="eliminarItem(${idItem})"><i class="bi bi-trash"></i></a>
+            <a onclick="eliminarItem('${idItem}')"><i class="bi bi-trash"></i></a>
         </li>`;
 
     let modalCarrito = document.getElementById("list");
     modalCarrito.innerHTML += item;
+}
 
+function editarCantidad(item,id){
+    let cantidad = parseInt(document.getElementById(item).value)
+    car.find(elemento => elemento.id = id).cantidad = cantidad
+    document.getElementById(item).value = cantidad
+    console.log(car)
 }
 
 function eliminarItem(id){
+    var newArray = car.filter((item) => item.id !== id);
+    car = newArray
 
+    localStorage.removeItem('carrito');
+    localStorage.setItem("carrito",JSON.stringify(car))
+
+    cantItems-= 1
+    localStorage.setItem("cantItems",cantItems) 
+
+    let spanCarrito = document.getElementById("itemsCar")
+    spanCarrito.innerHTML = cantItems
+    // agregar item visible a carrito
+    document.getElementById("list").innerHTML = ""
+    
+    if(car.length!==0){
+        car.forEach( elemnto => 
+            {
+                agregarItemAModal(elemnto.id, elemnto.descripcion, elemnto.cantidad, elemnto.precioUnitario)  
+            }
+        )
+        let total = 
+        `
+        <li class="listItems">
+            <div colspan="2"></div>
+            <span class="restricion"> TOTAL </span>
+            <span class="restricion">$${calcularTotal()} </span>
+        </li>
+        `
+        let modalCarrito = document.getElementById("list");
+        modalCarrito.innerHTML += total;
+    }
+    else{
+        document.getElementById("list").innerHTML = "No se ha seleccionado ningun producto. 😢"
+    }
 }
 
 const comentarios = document.getElementById("comentarios")
@@ -207,7 +262,7 @@ function guardarComentario(){
 function calcularTotal(){
     let total = 0;
     for( item in car){
-        total += car[item].cantidad * car[item].precioConIVA();
+        total += car[item].cantidad * car[item].precioUnitario;
     }
     return (total*monedaActual).toFixed(2);
 }
